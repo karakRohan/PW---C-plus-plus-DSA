b42f6836f23ea1f5dda3afb77c5663bc21b0b359
karakRohan <karakrohan2005@gmail.com>
2026-01-10 20:29:21 +0530

    add code

--- 6_Array/Lecture - 11/7_LinearSearch.cpp
+++ 6_Array/Lecture - 11/7_LinearSearch.cpp
@@ -4,25 +4,27 @@ using namespace std;
 int main()
 {
     int n;
-    cout<<"Enter Size Of Array : ";
-    cin>>n;
+    cout << "Enter Size Of Array : ";
+    cin >> n;
     int arr[n];
     // input
-    for(int i=0;i<=n-1;i++)
+    for (int i = 0; i <= n - 1; i++)
     {
-        cin>>arr[i];
+        cin >> arr[i];
     }
     int x;
-    cout<<"Enter The Element You Want To Search : ";
-    cin>>x;
+    cout << "Enter The Element You Want To Search : ";
+    cin >> x;
     // Search
     // check mark
     bool flag = false; // false means --> Element Is Not Present
-    for(int i=0;i<=n-1;i++)
+    for (int i = 0; i <= n - 1; i++)
     {
-        if(arr[i]==x) flag = true;
+        if (arr[i] == x)
+            flag = true;
     }
-    if(flag==true) cout<<"Element Found";
-    else cout<<"404 Element Not Found";
-     
+    if (flag == true)
+        cout << "Element Found";
+    else
+        cout << "404 Element Not Found";
 }

32f86eed45df64e8bc970eb4f5a2a74d448656c6
karakRohan <karakrohan2005@gmail.com>
2026-01-11 02:50:47 +0530

    Update Code

--- 16_Linked List/2_Linked List (Part - 2)/1_DeleteNode.cpp
+++ 16_Linked List/2_Linked List (Part - 2)/1_DeleteNode.cpp
@@ -1,49 +1,57 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-class Node{ // User defined data type
+class Node
+{ // User defined data type
 public:
     int val;
-    Node* next;
-    Node(int val){
+    Node *next;
+    Node(int val)
+    {
         this->val = val;
         this->next = NULL;
     }
 };
 
-void display(Node* head){
-    while(head!=NULL){
-        cout<<head->val<<" ";
+void display(Node *head)
+{
+    while (head != NULL)
+    {
+        cout << head->val << " ";
         head = head->next;
     }
-    cout<<endl;
+    cout << endl;
 }
 
-Node* deletenode(Node* head, int targetVal){
-    if(head->val==targetVal){
+Node *deletenode(Node *head, int targetVal)
+{
+    if (head->val == targetVal)
+    {
         head = head->next;
         return head;
     }
-    Node* temp = head;
-    while(temp->next->val!=targetVal){
+    Node *temp = head;
+    while (temp->next->val != targetVal)
+    {
         temp = temp->next;
     }
     temp->next = temp->next->next;
 }
 
-int main(){
-    Node* a = new Node(10);
-    Node* b = new Node(20);
-    Node* c = new Node(30);
-    Node* d = new Node(40);
-    Node* e = new Node(50);
+int main()
+{
+    Node *a = new Node(10);
+    Node *b = new Node(20);
+    Node *c = new Node(30);
+    Node *d = new Node(40);
+    Node *e = new Node(50);
 
     a->next = b;
     b->next = c;
     c->next = d;
     d->next = e;
-    Node* head = a;
+    Node *head = a;
 
     display(head);
-    head = deletenode(head,30);
+    head = deletenode(head, 30);
     display(head);
 }

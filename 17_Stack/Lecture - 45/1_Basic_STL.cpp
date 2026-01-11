3c11855dd4a5ede7caad4a5ceaa0a6aad6ea1edb
karakRohan <karakrohan2005@gmail.com>
2026-01-11 15:15:08 +0530

    Add code

---
+++ 17_Stack/Lecture - 45/1_Basic_STL.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+// #include<stack>
+using namespace std;
+int main(){
+    stack<int> stack1;
+    cout<<"Size of stack is: "<<stack1.size()<<endl;
+    stack1.push(10);    
+    stack1.push(20);
+    cout<<"Size of stack after pushing 2 elements is: "<<stack1.size()<<endl;
+}

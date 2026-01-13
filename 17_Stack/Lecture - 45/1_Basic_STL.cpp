d03769d08453c2819c7cc376d8f86381ffd7eee2
karakRohan <karakrohan2005@gmail.com>
2026-01-14 01:05:19 +0530

    add code

--- 17_Stack/Lecture - 45/1_Basic_STL.cpp
+++ 17_Stack/Lecture - 45/1_Basic_STL.cpp
@@ -9,6 +9,8 @@ int main()
     stack1.push(20);
     stack1.push(30);
     stack1.push(40);
+    stack1.push(50);
+
     // cout << "Size of stack after pushing 4 elements is: " << stack1.size() << endl;
     // stack1.pop();
     // cout << "Size of stack after popping 1 element is: " << stack1.size() << endl;
@@ -16,6 +18,12 @@ int main()
     // cout<<stack1.empty()<<endl;
     // return 0;
 
+    // printing in reverse order
+    while (stack1.size()>0)
+    {
+        cout << stack1.top() << " ";
+        stack1.pop();
+    }
 
 }
 

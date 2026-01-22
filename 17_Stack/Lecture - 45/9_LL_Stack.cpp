a9ebd732e1da970aa78a2c861beb99482e9c010d
karakRohan <karakrohan2005@gmail.com>
2026-01-23 00:15:10 +0530

    add code

---
+++ 17_Stack/Lecture - 45/9_LL_Stack.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{ // User defined data type
+public:
+    int val;
+    Node *next;
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+class Stack
+{ // User defined data structure
+public:
+    Node* head;
+    int size;
+    Stack()
+    {
+        head = NULL;
+        size = 0;
+    }
+    void push(int val)
+    {
+        Node* temp = new Node(val);
+        temp->next = head;
+        head = temp;
+        size++;
+    }
+    void pop()
+    {
+        if (head == NULL)
+        {
+            cout << "Stack is Empty !!!" << endl;
+            return;
+        }
+        head = head->next;
+        size--;
+    }
+    int top()
+    {
+        if (head == NULL)
+        {
+            cout << "Stack is Empty !!!" << endl;
+            return -1;
+        }
+        return head->val;
+    }
+    void display()
+    {
+        Node* temp = head;
+        while (temp != NULL)
+        {
+            cout << temp->val << " ";
+            temp = temp->next;
+        }
+        cout << endl;
+    }
+};
+
+int main()
+{
+    Stack stack1;
+    cout << stack1.top() << endl; // Stack is Empty !!!
+    stack1.push(10);
+    stack1.push(20);
+    stack1.push(30);
+    stack1.push(40);
+    stack1.push(50);
+    stack1.push(60);               // Stack is Full
+    cout << stack1.size << endl; // 5
+    cout << stack1.top() << endl;  // 50
+    stack1.display();              // 10 20 30 40 50
+}

adfa6a4db8ff2c7c23f14f89b5039fd431caf365
karakRohan <karakrohan2005@gmail.com>
2026-01-29 01:42:40 +0530

    refactor isBalanced function for readability and update test case

--- 17_Stack/Lecture - 46/1_Balance_Brackets.cpp
+++ 17_Stack/Lecture - 46/1_Balance_Brackets.cpp
@@ -1,13 +1,28 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-bool isBalanced(string s){
-
+bool isBalanced(string s)
+{
+    if (s.length() % 2 != 0) return false;
+    stack<int> st;
+    for (int i = 0; i < s.length(); i++)
+    {
+        if(s[i] == '(') st.push(s[i]);
+        else // s[i] == ')'
+        {
+            if(st.size()==0) return false;
+            else st.pop();
+        }
+    }
+    if(st.size()==0) return true;
+    else return false;
 }
 
 int main()
 {
-    string s = "()()()";
-    cout<<isBalanced(s)<<endl;
-
+    string s = "()()(";
+    cout << isBalanced(s) << endl;
 }
+
+// 1 = True
+// 0 = False

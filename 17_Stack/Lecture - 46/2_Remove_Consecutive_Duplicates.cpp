709b70d4d704ea46845acbd868285ed4bbc4721d
karakRohan <karakrohan2005@gmail.com>
2026-01-29 22:55:24 +0530

    add

---
+++ 17_Stack/Lecture - 46/2_Remove_Consecutive_Duplicates.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string removeDuplicates(string s)
+{
+    
+}
+
+int main()
+{
+    string s = "()()(";
+    cout << isBalanced(s) << endl;
+}
+
+// 1 = True
+// 0 = False

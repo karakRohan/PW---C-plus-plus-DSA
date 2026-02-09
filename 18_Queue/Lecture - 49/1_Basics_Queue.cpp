54194815fc281adc6b924a9928b84b96f1d5a6ab
karakRohan <karakrohan2005@gmail.com>
2026-02-09 15:08:37 +0530

    update: refactor display function for queue and clean up main function comments

--- 18_Queue/Lecture - 49/1_Basics_Queue.cpp
+++ 18_Queue/Lecture - 49/1_Basics_Queue.cpp
@@ -1,20 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
+void display(queue<int> q){
+    int n = q.size();
+    for(int i=1;i<=n;i++){
+        int x = q.front();
+        cout<<x<<" ";
+        q.pop();
+        q.push(x);
+    }
+    cout<<endl;
+}
 int main(){
     queue<int> q;
-    // push
-    //pop
-    // front -> top
-    //size, empty
-    // back
+    // push , pop , front , back , size , empty
     q.push(10);
     q.push(20);
     q.push(30);
     q.push(40);
     q.push(50);
-    cout<<"Front Element: "<<q.front()<<endl;
-    cout<<"Back Element: "<<q.back()<<endl;
-    cout<<"Size: "<<q.size()<<endl;
-    cout<<"Empty or not: "<<q.empty()<<endl;
+    // cout<<"Front Element: "<<q.front()<<endl;
+    // cout<<"Back Element: "<<q.back()<<endl;
+    // cout<<"Size: "<<q.size()<<endl;
+    // cout<<"Empty or not: "<<q.empty()<<endl;
+    display(q);
+    q.pop();
+    display(q);  
     
 }

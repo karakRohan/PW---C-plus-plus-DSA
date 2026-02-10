ad5cb66bfce7ac07936efc4bfdb6cc9b26e8a61b
karakRohan <karakrohan2005@gmail.com>
2026-02-10 22:51:14 +0530

    add: implement queue class with basic operations and display logic

---
+++ 18_Queue/Lecture - 49/3_Queue_From_Array.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Queue
+{
+public:
+    int f;
+    int b;
+    vector<int> arr;
+    Queue(int val)
+    {
+        f = 0;
+        b = 0;
+        vector<int> v(val);
+        arr = v;
+    }
+    void push(int val)
+    {
+        if (b == arr.size())
+        {
+            cout << "Queue is Full!" << endl;
+            return;
+        }
+        arr[b] = val;
+        b++;
+    }
+    void pop()
+    {
+        if (f - b == 0)
+        {
+            cout << "Queue is Empty!" << endl;
+            return;
+        }
+        f++;
+    }
+    int front()
+    {
+        if (f - b == 0)
+        {
+            cout << "Queue is Empty!" << endl;
+            return -1;
+        }
+        return arr[f];
+    }
+    int back()
+    {
+        if (f - b == 0)
+        {
+            cout << "Queue is Empty!" << endl;
+            return -1;
+        }
+        return arr[b - 1];
+    }
+    int size()
+    {
+        return b - f;
+    }
+    bool empty()
+    {
+        if (f - b == 0)
+            return true;
+        else
+            return false;
+    }
+    void display()
+    {
+        for (int i = f; i < b; i++)
+        {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
+    int main()
+    {
+        Queue q(5);
+        // push , pop , front , back , size , empty
+        q.push(10);
+        q.push(20);
+        q.push(30);
+        q.push(40);
+        q.display();
+        q.push(50);
+        q.push(60);
+        q.display();
+        q.pop();
+        q.display();
+    }

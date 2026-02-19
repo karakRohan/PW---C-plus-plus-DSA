cb643e18db1a51c73bef0bca22405f6d109f4ae3
karakRohan <karakrohan2005@gmail.com>
2026-02-19 22:09:19 +0530

    add

---
+++ 19_Binary Trees/Lecture - 53/3_BFS_Traversals.cpp
@@ -0,0 +1,59 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node{ // This is a TreeNode 
+public:
+    int val; 
+    Node* left;
+    Node* right;
+    Node(int val){
+        this->val=val;
+        this->left=NULL;
+        this->right=NULL;
+    }
+};
+
+int levels(Node* root){
+    if(root==NULL) return 0;
+    return 1+max(levels(root->left), levels(root->right));
+}
+
+void nthLevel(Node* root, int curr, int level){
+    if(root==NULL) return; // base case
+    if(curr==level){
+        cout<<root->val<<" "; // root
+        return;
+    }
+    nthLevel(root->left, curr+1, level); // left
+    nthLevel(root->right, curr+1, level); // right
+}
+
+void levelOrder(Node* root){
+    int n = levels(root);
+    for(int i=1; i<=n; i++){
+        nthLevel(root, 1, i);
+        cout<<endl;
+    }
+}
+
+int main(){
+    Node* a = new Node(1); // root node
+    Node* b = new Node(2);
+    Node* c = new Node(3);      
+    Node* d = new Node(4);
+    Node* e = new Node(5);
+    Node* f = new Node(6);
+    Node* g = new Node(7); 
+
+    a->left=b;
+    a->right=c;
+    b->left=d;
+    b->right=e;
+    c->left=f;
+    c->right=g;
+    
+    // cout<<endl;
+    // nthLevel(a, 1,2);
+    // nthLevel(a, 1,3);
+    // nthLevel(a, 1,4);
+    levelOrder(a);
+}

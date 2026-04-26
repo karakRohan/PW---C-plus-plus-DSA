6df3baea37c94b9231e6cdad2471a326ec791fe7
karakRohan <karakrohan2005@gmail.com>
2026-04-26 13:05:38 +0530

    add

---
+++ 26_Backtracking/6_1804_Implement_Trie_II_Prefix_Tree.cpp
@@ -0,0 +1,96 @@
+// Question Name : 1804. Implement Trie II (Prefix Tree)
+// Question Link : https://leetcode.com/problems/implement-trie-ii-prefix-tree/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node {
+public:
+    char data;
+    bool terminal;
+    unordered_map<char, Node*> children;
+    int countWordsEndingHere;
+    int countWordsStarting;
+
+    Node(char data) {
+        this->data = data;
+        this->terminal = false;
+        this->countWordsEndingHere = this->countWordsStarting = 0;
+    }
+};
+
+class Trie {
+public:
+    Node* root;
+
+    Trie() {
+        root = new Node('\0');
+    }
+
+    void insert(string word) {
+        Node* curr = root;
+
+        for (int i = 0; i < word.size(); i++) {
+            char ch = word[i];
+
+            if (curr->children.count(ch)) {
+                curr->countWordsStarting++;
+                curr = curr->children[ch];
+            } else {
+                Node* child = new Node(ch);
+                curr->children[ch] = child;
+                curr->countWordsStarting++;
+                curr = curr->children[ch];
+            }
+        }
+
+        curr->terminal = true;
+        curr->countWordsEndingHere++;
+    }
+
+    int countWordsEqualTo(string prefix) {
+    Node* curr = root;
+
+        for (int i = 0; i < prefix.size(); i++) {
+            char ch = prefix[i];
+
+            if (curr->children.count(ch)) {
+                curr = curr->children[ch];
+            } else {
+                return 0;
+            }
+        }
+
+        return curr->countWordsEndingHere;
+    }
+    int countWordsStartingWith(string prefix) {
+    Node* curr = root;
+
+        for (int i = 0; i < prefix.size(); i++) {
+            char ch = prefix[i];
+
+            if (curr->children.count(ch)) {
+                curr = curr->children[ch];
+            } else {
+                return 0;
+            }
+        }
+
+        return curr->countWordsStarting;
+    }
+    void erase(string word) {
+    Node* curr = root;
+
+        for (int i = 0; i < word.size(); i++) {
+            char ch = word[i];
+            curr->countWordsStarting--;
+            curr = curr->children[ch];
+        }
+
+        curr->countWordsEndingHere--;
+    }
+};
+
+int main(){
+    return 0;
+}
